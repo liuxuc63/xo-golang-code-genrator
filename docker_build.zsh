5b9d1c7fd7b8b13d4e94f34a9e00019363d8d789
softlang-net <x915927@icloud.com>
2022-10-15 16:26:57 +0800

    Update docker_build.zsh
    run xo on alpine by docker

--- docker_build.zsh
+++ docker_build.zsh
@@ -3,7 +3,8 @@ set -e
 
 docker run -itd --rm --name go19 -m 2048M -v $PWD:/app golang:alpine3.16
 
-#docker run -itd --rm --name go19 -m 2048M -v $PWD:/app golang:alpine3.16
+# docker run -itd --rm --name xo -m 1024M -v $PWD:/app -w /app alpine:3.16
+# docker run -itd --rm --name go19 -m 2048M -v $PWD:/app golang:alpine3.16
 # ----- proxy & dependency
 
 # https://github.com/mattn/go-sqlite3
